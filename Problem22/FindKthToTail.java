c7faa9a0812ed7409b895de2eaddc10850205dad
cirs1313 <zhongqiangxue@163.com>
2017-09-18 15:31:34 +0800

    Problem22

--- Problem22/FindKthToTail.java
+++ Problem22/FindKthToTail.java
@@ -4,17 +4,17 @@
 public class FindKthToTail {
     public ListNode find(ListNode head, int k) {
         if (head == null || k <= 0) return null;
-        ListNode node1, node2;
+        ListNode node1, nodeBehind;
         node1 = head;
-        node2 = head;
+        nodeBehind = head;
         for (int i = 0; i < k - 1; i++) {
             if (node1.next != null) node1 = node1.next;
             else return null;
         }
         while (node1.next != null) {
             node1 = node1.next;
-            node2 = node2.next;
+            nodeBehind = nodeBehind.next;
         }
-        return node2;
+        return nodeBehind;
     }
 }
